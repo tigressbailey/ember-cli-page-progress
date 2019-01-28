2eeb8e9c5322e124d19fa5fe89cf0720eec6f7b0
Donald Wasserman <djwasserman@gmail.com>
2019-01-28 05:37:32 -0500

    Fixed copy paste error

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// For ember-cli < 2.7 findHost doesnt exist so we backport from that version
+// for earlier version of ember-cli.
+// https://github.com/ember-cli/ember-cli/blame/16e4492c9ebf3348eb0f31df17215810674dbdf6/lib/models/addon.js#L533
+function findHostShim() {
+  let current = this;
+  let app;
+  do {
+    app = current.app || app;
+  } while (current.parent.parent && (current = current.parent));
+  return app;
+}
+
 module.exports = {
   name: 'ember-cli-page-progress',
 
